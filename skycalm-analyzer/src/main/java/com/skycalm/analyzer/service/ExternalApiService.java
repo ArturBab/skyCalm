70595f3e392e335e740ff24644f737d72afb1ada
ArturBab <90608148+ArturBab@users.noreply.github.com>
2025-04-08 16:07:07 +0500

    Update 1.1

---
+++ skycalm-analyzer/src/main/java/com/skycalm/analyzer/service/ExternalApiService.java
@@ -0,0 +1,22 @@
+package com.skycalm.analyzer.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExternalApiService {
+
+    public Map<String, String> fetchFLightAnalysis(String flightNumber, String departureDate) {
+        
+        // Простая заглушка
+
+        Map<String, String> data = new HashMap<>();
+        data.put("routeSummary", "Москва — Нью-Йорк");
+        data.put("turbulenceInfo", "Средняя турбулентность");
+        data.put("weatherInfo", "Переменная облачность");
+        data.put("message", "Информация получена успешно");
+        data.put("recommendation", "Выберите место у прохода");
+
+        System.out.println(" Запрос к внешнему API (заглушка): " + flightNumber + ", " + departureDate);
+        return data;
+    }
+}
--- skycalm-analyzer/src/main/java/com/skycalm/analyzer/service/FlightAnalyzerServiceImpl.java
+++ skycalm-analyzer/src/main/java/com/skycalm/analyzer/service/FlightAnalyzerServiceImpl.java
@@ -1,29 +1,44 @@
 package com.skycalm.analyzer.service;
 
+import java.util.Map;
+
 import com.skycalm.analyzer.grpc.FlightRequest;
 import com.skycalm.analyzer.grpc.FlightResponse;
 import com.skycalm.analyzer.grpc.FlightAnalyzerServiceGrpc.FlightAnalyzerServiceImplBase;
 
 import io.grpc.stub.StreamObserver;
 
-
 public class FlightAnalyzerServiceImpl extends FlightAnalyzerServiceImplBase {
-    
+
+    private final ExternalApiService apiService = new ExternalApiService();
+
     @Override
     public void analyzeFlight(FlightRequest request, StreamObserver<FlightResponse> responseObserver) {
 
-        // Простая заглушка
+        String flightNumber = request.getFlightNumber();
+        String departureDate = request.getDepartureDate();
+
         System.out.println("Получен запрос: " + request.getFlightNumber() + " | " + request.getDepartureDate());
 
+        Map<String, String> apiData = apiService.fetchFLightAnalysis(flightNumber, departureDate);
+
         FlightResponse response = FlightResponse.newBuilder()
-            .setRouteSummary("Маршрут пока не определён")
-            .setTurbulenceInfo("Нет данных")
-            .setWeatherInfo("Без осадков")
-            .setMessage("Запрос обработан успешно")
-            .setRecommendation("Оставайтесь на связи")
-            .build();
-     
+                /*
+                 * Пример пустой заглушки
+                 * .setRouteSummary("Маршрут пока не определён")
+                 * .setTurbulenceInfo("Нет данных")
+                 * .setWeatherInfo("Без осадков")
+                 * .setMessage("Запрос обработан успешно")
+                 * .setRecommendation("Оставайтесь на связи")
+                 */
+                .setRouteSummary(apiData.get("routeSummary"))
+                .setTurbulenceInfo(apiData.get("turbulenceInfo"))
+                .setWeatherInfo(apiData.get("weatherInfo"))
+                .setMessage(apiData.get("message"))
+                .setRecommendation(apiData.get("recommendation"))
+                .build();
+
         responseObserver.onNext(response);
-        responseObserver.onCompleted();            
+        responseObserver.onCompleted();
     }
 }
